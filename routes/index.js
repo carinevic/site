a613a4f0ddb8be185286d09731ecb5790616b184
Carine Ngwekwe <carine@carines-mbp.attlocal.net>
2019-10-11 20:12:40 -0500

    site

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+const hotelController = require('../controllers/hotelController');
+
+/* GET home page. */
+router.get('/', hotelController.homePage);
+ 
+
+router.get('/all', function(req, res){
+  res.render('all_hotels', {title: 'All Hotels' });
+
+});
+
+module.exports = router;
